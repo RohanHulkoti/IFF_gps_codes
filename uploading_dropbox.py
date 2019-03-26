e61a53218c5dbc56a6d3c401025b1c8313240fcd
RohanHulkoti <48554214+RohanHulkoti@users.noreply.github.com>
2019-03-26 11:26:56 +0100

    Update uploading_dropbox.py

--- uploading_dropbox.py
+++ uploading_dropbox.py
@@ -7,6 +7,6 @@ starttime = time.time()
 while True:
     if time.time() >= starttime + 300:
 
-	os.system('./dropbox_uploader.sh delete /dropbox/log_temp2.csv')
-	os.system('./dropbox_uploader.sh upload log_temp2.csv /dropbox/')
+	os.system('./dropbox_uploader.sh delete /drone_gps_backup/log_temp2.csv')
+	os.system('./dropbox_uploader.sh upload log_temp2.csv /drone_gps_backup/')
 	starttime = time.time()
